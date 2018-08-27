eceba8130b7c2bee7a8f78ca2dd2dc39dbd65956
shreypasari-accolite <shrey.pasari@accoliteindia.com>
2018-08-27 12:58:56 +0530

    Adding service for mysql query execution

--- src/main/java/com/heteroDB/metaDataStore/controller/QueryExecutionController.java
+++ src/main/java/com/heteroDB/metaDataStore/controller/QueryExecutionController.java
@@ -1,5 +1,6 @@
 package com.heteroDB.metaDataStore.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,13 +8,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.heteroDB.metaDataStore.databaseServiceClient.MysqlServiceClient;
+import com.heteroDB.metaDataStore.enums.DatabaseServers;
 import com.heteroDB.metaDataStore.model.QueryData;
 
 @RestController
 public class QueryExecutionController {
 	
+	@Autowired
+	MysqlServiceClient mysqlServiceClient;
+	
 	@RequestMapping(value = "/executeQuery", method = RequestMethod.POST)
 	public ResponseEntity<Void> ExecuteQuery(@RequestBody QueryData queryData) {
+		DatabaseServers dbServers = queryData.getDatabaseType();
+		switch(dbServers) {
+		case mysql:
+			mysqlServiceClient.executeMysqlQuery(queryData.getScript());
+		}
+		
 		System.out.println(queryData);
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
--- src/main/java/com/heteroDB/metaDataStore/model/QueryData.java
+++ src/main/java/com/heteroDB/metaDataStore/model/QueryData.java
@@ -1,9 +1,11 @@
 package com.heteroDB.metaDataStore.model;
 
+import com.heteroDB.metaDataStore.enums.DatabaseServers;
+
 public class QueryData {
 	
 	private String script;
-	private String databaseType;
+	private DatabaseServers databaseType;
 	
 	
 	
@@ -14,14 +16,14 @@ public class QueryData {
 	public void setScript(String script) {
 		this.script = script;
 	}
-	public String getDatabaseType() {
+	
+	
+	public DatabaseServers getDatabaseType() {
 		return databaseType;
 	}
-	public void setDatabaseType(String databaseType) {
+	public void setDatabaseType(DatabaseServers databaseType) {
 		this.databaseType = databaseType;
 	}
-	
-	
 	@Override
 	public String toString() {
 		return "QueryData [script=" + script + ", databaseType=" + databaseType + "]";
